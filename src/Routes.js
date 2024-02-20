62f756748e816c8dacb33363c63f92536838a734
Shelendra <singhshelendra70@gmail.com>
2024-02-21 00:16:14 +0530

    project upload

---
+++ src/Routes.js
@@ -0,0 +1,7 @@
+import LoginPage from '../src/pages/Login.jsx';
+import SignupPage  from '../src/pages/Signup.jsx'
+
+export {
+    LoginPage,
+    SignupPage
+}
---
+++ src/components/Signup/SignupPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Signup from '../../pages/Signup'
+
+const LoginPage = () => {
+  return (
+    <div>
+     <Signup/>
+    </div>
+  )
+}
+
+export default LoginPage
